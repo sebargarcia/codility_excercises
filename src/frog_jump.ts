7326473ca2b4c0ec039b675acbb74630ca29a87e
Sebastián Garcia <sebargarcia@gmail.com>
2021-01-27 22:16:19 -0300

    📝 Updated excercises with problem statement

--- src/frog_jump.ts
+++ src/frog_jump.ts
@@ -1,20 +1,31 @@
+// A small frog wants to get to the other side of the road. The frog is
+//currently located at position X and wants to get to a position greater
+//than or equal to Y. The small frog always jumps a fixed distance, D.
+// Count the minimal number of jumps that the small frog must perform to reach its target.
+// Write a function:
+//     class Solution { public int solution(int X, int Y, int D); }
+// that, given three integers X, Y and D, returns the minimal number of jumps
+//from position X to a position equal to or greater than Y.
+// For example, given:
+//   X = 10
+//   Y = 85
+//   D = 30
+// the function should return 3, because the frog will be positioned as follows:
+//         after the first jump, at position 10 + 30 = 40
+//         after the second jump, at position 10 + 30 + 30 = 70
+//         after the third jump, at position 10 + 30 + 30 + 30 = 100
+// Write an efficient algorithm for the following assumptions:
+//         X, Y and D are integers within the range [1..1,000,000,000];
+//         X ≤ Y.
+
+//*******SOLUTION STARTS HERE **********/
+
 //X:initial position
 //Y:Final position (equals o greater than Y)
 //D: Jump Fixed distance
 function solution(X: number, Y: number, D: number) {
   return (Y - X) % D === 0 ? (Y - X) / D : Math.floor((Y - X) / D) + 1;
 }
-
-// For example, given:
-//   X = 10
-//   Y = 85
-//   D = 30
-
-// the function should return 3, because the frog will be positioned as follows:
-
-//         after the first jump, at position 10 + 30 = 40
-//         after the second jump, at position 10 + 30 + 30 = 70
-//         after the third jump, at position 10 + 30 + 30 + 30 = 100
 
 const frogJump = () => console.log(solution(10, 85, 30));
 export default frogJump;
